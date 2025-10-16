b438ac56d7a2d103ef3b8cc0072b4d9f3689573d
Neelam Raikwar <neelam.raikwar.234303@gmail.com>
2025-10-17 02:36:58 +0530

    feat : Added Filter by priority

--- src/components/LeadStatusView.jsx
+++ src/components/LeadStatusView.jsx
@@ -1,9 +1,122 @@
-const LeadStatusView = () => {
-    return (
-        <main>
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
-        </main>
-    )
-}
+const LeadStatusView = () => {
+  const [proposal, setProposal] = useState();
+  const [leads, setLeads] = useState();
+  const [filteredLeads, setFilteredLeads] = useState();
+
+  useEffect(() => {
+    async function fetchQualifiedLeads() {
+      const qualfydStatus = await axios.get(
+        "https://anvaya-model-references-apis-backen.vercel.app/leads/status/Qualified"
+      );
+      console.log(qualfydStatus.data, "qualfydStatus");
+      setProposal(qualfydStatus.data);
+    }
+    fetchQualifiedLeads();
+  }, []);
+
+  useEffect(() => {
+    async function fetchLeads() {
+      const fetchLeads = await axios.get(
+        "https://anvaya-model-references-apis-backen.vercel.app/leads"
+      );
+      console.log("leads", fetchLeads.data);
+      setLeads(fetchLeads.data);
+    }
+    fetchLeads();
+  }, []);
+
+  //   console.log(filteredLeads, "filterLeads");
+
+  function handleInputChange(value) {
+    const filtered = leads?.filter((lead) => lead.priority === value);
+    setFilteredLeads(filtered, "filtered");
+    console.log(filtered, "checking leads");
+  }
+
+  return (
+    <main className="leadContainer">
+      <h1 className="text">Lead by Status</h1>
+      <div className="container">
+        <div className="backButn">
+          <Link to="/">
+            <button>Back to Dashboard</button>
+          </Link>
+        </div>
+
+        <div className="midContainer">
+          <h2>Lead List by Status</h2>
+          <section>
+            <h3>Status: Qualified</h3>
+            <div style={{ border: "1px solid red" }}>
+              {proposal?.map((prop, index) => (
+                <div key={prop._id} style={{ border: "1px" }}>
+                  <p>
+                    <strong>Lead {index + 1}</strong> &nbsp; - &nbsp;
+                    <strong>Lead Name: </strong>&nbsp;{prop.name}&nbsp; &nbsp;
+                    <strong>Status: </strong>&nbsp;{prop.status} &nbsp; &nbsp;{" "}
+                    <strong>Sales Agent Name:</strong> &nbsp;{" "}
+                    {prop.salesAgent?.name}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </section>
+          <br />
+          <section>
+            <h2>Leads Filtered by Priority</h2>
+            {filteredLeads?.map((lead) => (
+              <div key={lead._id}>
+                <p>
+                  Lead Name: <strong>{lead.name}</strong> - Priority :{" "}
+                  {lead.priority}
+                </p>
+              </div>
+            ))}
+          </section>
+
+          <section>
+            <h2>Filter by Priority</h2>
+            <div style={{ display: "flex", gap: "6rem" }}>
+              <label htmlFor="h">
+                <input
+                  type="checkbox"
+                  id="h"
+                  name="priority"
+                  value="High"
+                  onChange={(e) => handleInputChange(e.target.value)}
+                />
+                High
+              </label>
+              <label htmlFor="m">
+                <input
+                  type="checkbox"
+                  id="m"
+                  name="priority"
+                  value="Medium"
+                  onChange={(e) => handleInputChange(e.target.value)}
+                />
+                Medium
+              </label>
+              <label htmlFor="l">
+                <input
+                  type="checkbox"
+                  id="l"
+                  name="priority"
+                  value="Low"
+                  onChange={(e) => handleInputChange(e.target.value)}
+                />
+                Low
+              </label>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  );
+};
 
 export default LeadStatusView;
--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -1,10 +1,106 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Chart as ChartJS } from "chart.js/auto";
+import { Bar, Doughnut, Pie } from "react-chartjs-2";
+import axios from "axios";
 
 const Report = () => {
-    return (
-        <main className="leadContainer">
-        <h1 className="text">Anvaya CRM Reports </h1>
-         <div className="container">
+  const [leads, setLeads] = useState([]);
+  const [closedLead, setClosedLead] = useState([]);
+  const [leadsInPipeLine, setLeadsInPipeLine] = useState(0);
+  const [closedLeadsInPipeline, setClosedLeadsInPipeline] = useState(0);
+
+  //leads by statuses;
+  const [newLead, setNewLead] = useState([]);
+  const [qualify, setQualify] = useState([]);
+  const [proposal, setProposal] = useState([]);
+
+  console.log(closedLead, "chekcing");
+  console.log(leads, "checking leads");
+
+  const AllLeadApi =
+    "https://anvaya-model-references-apis-backen.vercel.app/leads";
+
+  useEffect(() => {
+    async function getAllLeadApi() {
+      try {
+        const res = await axios.get(AllLeadApi);
+        console.log(res.data, "Fetching leads. ");
+        setLeads(res.data);
+      } catch (error) {
+        console.log("Error message: ", error.message);
+      }
+    }
+    getAllLeadApi();
+  }, []);
+
+  useEffect(() => {
+    const fetchClosedLeads = async () => {
+      const res = await axios.get(
+        "https://anvaya-model-references-apis-backen.vercel.app/leads/status/Closed"
+      );
+      console.log(res.data, "checkig res");
+      setClosedLead(res.data);
+    };
+    fetchClosedLeads();
+  }, []);
+
+  useEffect(() => {
+    async function fetchingTotalInPipeLine() {
+      try {
+        const totalLeads = await axios.get(
+          "https://anvaya-model-references-apis-backen.vercel.app/leads/report/pipeline"
+        );
+        console.log(totalLeads.data.totalLeadsInPipeline, "totalLeads");
+        setLeadsInPipeLine(totalLeads.data.totalLeadsInPipeline);
+
+        const totalClosedLeads = await axios.get(
+          "https://anvaya-model-references-apis-backen.vercel.app/leads/report/closedLeads/pipeline"
+        );
+        console.log(
+          totalClosedLeads.data.totalClosedLeadsInPipeline,
+          "totalClosedLeads"
+        );
+        setClosedLeadsInPipeline(
+          totalClosedLeads.data.totalClosedLeadsInPipeline
+        );
+      } catch (error) {
+        console.log(error, "Error in the pipeline.");
+      }
+    }
+    fetchingTotalInPipeLine();
+  }, []);
+
+  useEffect(() => {
+    async function leadsByStatuses() {
+      try {
+        const newStatus = await axios.get(
+          "https://anvaya-model-references-apis-backen.vercel.app/leads/status/New"
+        );
+        console.log(newStatus, "newStatus");
+        setNewLead(newStatus.data);
+        const proposalStatus = await axios.get(
+          "https://anvaya-model-references-apis-backen.vercel.app/leads/status/Proposal_Sent"
+        );
+        console.log(proposalStatus, "proposalStatus");
+        setQualify(proposalStatus.data);
+        const qualfydStatus = await axios.get(
+          "https://anvaya-model-references-apis-backen.vercel.app/leads/status/Qualified"
+        );
+        console.log(qualfydStatus.data, "qualfydStatus");
+        setProposal(qualfydStatus.data);
+      } catch (error) {
+        throw error;
+      }
+    }
+
+    leadsByStatuses();
+  }, []);
+
+  return (
+    <main className="leadContainer">
+      <h1 className="text">Anvaya CRM Reports </h1>
+      <div className="container">
         <div className="backButn">
           <Link to="/">
             <button>Back to Dashboard</button>
@@ -14,26 +110,109 @@ const Report = () => {
         <div className="midContainer">
           <h2>Report Overview</h2>
           <section>
-          <p>Total Leads closed and in Pipeline: [Pie Chart]|</p>
+            <p style={{ fontSize: "20px" }}>
+              <strong>Total closed Leads and Leads in Pipeline: </strong>
+            </p>
+
+            <div
+              style={{
+                border: "1px solid red",
+                width: "30rem",
+                height: "30rem",
+              }}
+            >
+              <Doughnut
+                data={{
+                  labels: ["Leads in Pipeline", "Closed Leads in Pipeline"],
+                  datasets: [
+                    {
+                      label: "Leads",
+                      data: [leadsInPipeLine, closedLeadsInPipeline],
+                      // backgroundColor: "rgba(75,192,192,0.6)",
+
+                      backgroundColor: [
+                        "rgba(54, 162, 235, 0.6)", // Blue for pipeline
+                        "rgba(75, 192, 192, 0.6)", // Teal for closed
+                      ],
+                      borderWidth: 1,
+                    },
+                  ],
+
+                  // labels: ,
+                  // datasets: [
+                  //   {
+                  //     label: "Total Closed Leads in the Pipeline",
+                  //     data : closedLead?.map((lead) => lead?.timeToClose) || [],
+                  //     backgroundColor: "rgba(75,192,192,0.6)",
+                  //   }
+                  // ]
+                }}
+              />
+            </div>
           </section>
 
           <section>
-        <p>Leads Closed by Sales Agent:</p>
-        <p>[Bar Chart]</p>
-        </section>
+            <p style={{ fontSize: "20px" }}>
+              <strong>Leads Closed by Sales Agent:</strong>
+            </p>
+            {/* <p>[Bar Chart]</p> */}
+            <div
+              style={{
+                border: "1px solid red",
+                width: "50rem",
+                height: "30rem",
+              }}
+            >
+              <Bar
+                data={{
+                  labels: closedLead?.map((lead) => lead?.name) || [],
+                  datasets: [
+                    {
+                      label: "Closed Leads",
+                      data: closedLead?.map((lead) => lead?.timeToClose) || [],
+                      backgroundColor: "rgba(75,192,192,0.6)",
+                      borderRadius: "5",
+                    },
+                  ],
+                }}
+              />
+            </div>
+          </section>
           <br />
 
           <section>
-            <p> Lead Status Distribution: </p>
-            <p> [Pie Chart or Bar Chart]</p>
+            <p style={{ fontSize: "20px" }}>
+              <strong>Lead Status Distribution:</strong>
+            </p>
+            <div
+              style={{
+                border: "1px solid red",
+                width: "30rem",
+                height: "30rem",
+              }}
+            >
+              <Doughnut
+                data={{
+                  labels: ["Proposal Sent", "Qualified", "New"],
+                  datasets: [
+                    {
+                      label: "Leads Status Distribution",
+                      data: [newLead.length, qualify.length, proposal.length],
+                      backgroundColor: [
+                        "rgba(75, 192, 192, 0.6)", // Closed color
+                        "rgba(54, 162, 235, 0.6)", // Pipeline color
+                        "rgba(255, 206, 86, 0.6)", // In Progress color
+                      ],
+                    },
+                  ],
+                }}
+              />
+            </div>
           </section>
-          
         </div>
       </div>
-
-
-        </main>
-    )
-}
+    </main>
+  );
+};
 
 export default Report;
